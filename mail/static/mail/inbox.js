d26f1d0e0dbc57e29d86a6240edc0992cf5be755
Xu Che <xuche1998@gmail.com>
2022-09-22 16:13:40 +0800

    bootstrap styling + indv email view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -48,22 +48,46 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
-      console.log(emails)
-      emails.forEach(display_email);
+      // console.log(emails)
+      emails.forEach(display_inbox);
     });
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-indv-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
 }
 
-function display_email(contents) {
+function display_inbox(contents) {
   const post = document.createElement('div');
-  post.className = 'email';
-  post.innerHTML = contents['body'];
-
+  post.className = 'email-entry';
+  post.innerHTML = '<ul class="list-group list-group-horizontal border border-dark rounded-0">'
+    + '<li class="list-group-item col-3 border-0 font-weight-bold  text-left">' + contents.sender
+    + '</li><li class="list-group-item col-6 border-0">' + contents.subject
+    + '</li><li class="list-group-item col-3 border-0 text-right">' + contents.timestamp
+    + '</li></ul>';
+  post.addEventListener('click', () => open_email(contents.id));
   document.querySelector('#emails-view').append(post)
+}
+
+function open_email(email_id) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-indv-view').style.display = 'block';
+
+  fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    .then(email => {
+      console.log(email)
+      document.querySelector('#email-indv-view').innerHTML = `<p><span class="font-weight-bold">From: </span>${email.subject}</p>
+      <p><span class="font-weight-bold">To: </span>${email.recipients}</p>
+      <p><span class="font-weight-bold">Subject: </span>${email.subject}</p>
+      <p><span class="font-weight-bold">Timestamp: </span>${email.timestamp}</p>
+      <hr><p>${email.body}</p>
+      `;
+    });
 }
